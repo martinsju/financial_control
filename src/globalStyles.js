2ba9e4813e57b09a94b08c3d21f24efe4aac69f6
julia <agramartinsj@gmail.com>
2022-10-17 17:35:55 -0300

    style: created global style

---
+++ src/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    
+    body{
+        background-color: #292939;
+        color: #f7f7f7;
+        font-family: 'Montserrat Alternates', sans-serif;
+    }
+`
