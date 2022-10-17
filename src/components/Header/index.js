9ffa8bc73f1f65ccf99dc630e60eed063007cd81
julia <agramartinsj@gmail.com>
2022-10-17 17:38:35 -0300

    feat: created Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as C from './styles.js'
+
+function Header() {
+	return (
+		<C.Container>
+			<C.Title>Financial Control App</C.Title>
+		</C.Container>
+	)
+}
+
+export default Header
