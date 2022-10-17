eb29bf02fc69525d3228940e39b58d5936b1681e
julia <agramartinsj@gmail.com>
2022-10-17 17:39:00 -0300

    style: Header styling

---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	height: 150px;
+	text-align: center;
+	background-color: #1b1b2b;
+`
+
+export const Title = styled.h1`
+	font-weight: 300;
+	padding-top: 20px;
+`
