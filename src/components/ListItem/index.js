15853efa80cb74708b051d8ce212d0aded572489
julia <agramartinsj@gmail.com>
2022-10-19 12:38:22 -0300

    feat: added onDelete function

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,9 +1,29 @@
 import * as C from './styled.js'
+import {
+	FaTrash,
+	FaRegArrowAltCircleUp,
+	FaRegArrowAltCircleDown
+} from 'react-icons/fa'
 
 import React from 'react'
 
-function ListItem(props) {
-	return <C.List>{props.children}</C.List>
+function ListItem({ item, onDelete }) {
+	return (
+		<C.List>
+			<C.Label>{item.description}</C.Label>
+			<C.Label>R${item.amount}</C.Label>
+			<C.Label width='50%' justifyContent='center'>
+				{item.isExpense ? (
+					<FaRegArrowAltCircleUp color='red' />
+				) : (
+					<FaRegArrowAltCircleDown color='green' />
+				)}
+			</C.Label>
+			<C.Label width='10%' justifyContent='center'>
+				<FaTrash onClick={() => onDelete(item.id)} />
+			</C.Label>
+		</C.List>
+	)
 }
 
 export default ListItem
--- src/components/TransactionsList/index.js
+++ src/components/TransactionsList/index.js
@@ -2,30 +2,30 @@ import React from 'react'
 import * as C from './styled.js'
 import ListItem from '../ListItem/index.js'
 
-function TransactionsList({ list }) {
+function TransactionsList({ list, setList }) {
+	function onDelete(id) {
+		console.log('deletando item ', id)
+		const newList = list.filter((item) => item.id !== id)
+		setList(newList)
+		localStorage.setItem('transactions', JSON.stringify(newList))
+	}
+
 	return (
 		<C.Container>
-			<C.Section>
-				<C.Label>Income</C.Label>
-				{list
-					.filter((item) => !item.isExpense)
-					.map((item) => (
-						<ListItem key={item.id}>
-							{item.description} R${item.amount}
-						</ListItem>
-					))}
-			</C.Section>
+			<C.TitleSection>
+				<C.Title>Description</C.Title>
+				<C.Title>Value</C.Title>
+				<C.Title width='50%' justifyContent='center'>
+					Type
+				</C.Title>
+				<C.Title width='10%'></C.Title>
+			</C.TitleSection>
 
-			<C.Section>
-				<C.Label>expense</C.Label>
-				{list
-					.filter((item) => item.isExpense)
-					.map((item) => (
-						<ListItem key={item.id}>
-							{item.description} R${item.amount}
-						</ListItem>
-					))}
-			</C.Section>
+			<C.ItemsSection>
+				{list.map((item) => (
+					<ListItem key={item.id} item={item} onDelete={onDelete}></ListItem>
+				))}
+			</C.ItemsSection>
 		</C.Container>
 	)
 }
