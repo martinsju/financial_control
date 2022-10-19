4492532d403c619b27890a588e5ca4d2ef08bd0b
julia <agramartinsj@gmail.com>
2022-10-19 12:39:49 -0300

    style: refactor list

--- src/components/TransactionsList/styled.js
+++ src/components/TransactionsList/styled.js
@@ -9,25 +9,38 @@ export const Container = styled.div`
 	box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
 	border-radius: 5px;
 	display: flex;
-	justify-content: space-between;
+	flex-direction: column;
 	box-sizing: border-box;
-	padding: 15px 15px;
-	gap: 60px;
+	padding: 20px 25px;
+	gap: 15px;
 
 	@media (max-width: 750px) {
 		display: block;
 	}
 `
 
-export const Section = styled.div`
-	width: 50%;
+export const Title = styled.h2`
+	width: ${(props) => (props.width ? props.width : '100%')};
+	font-weight: 700;
+	font-size: 18px;
+	display: flex;
+	justify-content: ${(props) =>
+		props.justifyContent ? props.justifyContent : 'start'};
+	padding-bottom: 5px;
+`
+
+export const TitleSection = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	gap: 15px;
+	border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+`
+
+export const ItemsSection = styled.div`
+	width: 100%;
 	display: flex;
 	flex-direction: column;
-	gap: 10px;
-`
-export const Label = styled.label`
-	font-weight: 700;
-	font-size: 20px;
-	padding-bottom: 5px;
-	border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+	gap: 15px;
 `
