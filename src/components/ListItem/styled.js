20697ef99fe74d99027c8193f6c833030bac4d88
julia <agramartinsj@gmail.com>
2022-10-19 12:40:25 -0300

    style: created conditional label and li style

--- src/components/ListItem/styled.js
+++ src/components/ListItem/styled.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components'
 
 export const List = styled.li`
+	display: flex;
+	justify-content: space-between;
+	gap: 15px;
 	list-style: none;
+`
+
+export const Label = styled.label`
+	width: ${(props) => (props.width ? props.width : '100%')};
+	color: ${(props) => (props.color ? props.color : '')};
+	font-weight: 400;
+	font-size: 16px;
+	display: flex;
+	justify-content: ${(props) =>
+		props.justifyContent ? props.justifyContent : 'start'};
+	align-items: center;
+
+	svg {
+		width: 18px;
+		height: 18px;
+	}
 `
